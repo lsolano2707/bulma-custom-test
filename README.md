717a07536f9897e1d4841afc798fb2adf74fd287
Leonardo Solano <lsolano@wfscorp.com>
2022-03-13 18:41:32 -0500

    implement Sass 7-1 Pattern

--- README.md
+++ README.md
@@ -1,29 +1,5 @@
 # bulma-custom-test
 
-## Project setup
-```
-npm install
-```
+## documentation
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Run your unit tests
-```
-npm run test:unit
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+https://bulma.io/documentation/customize/with-node-sass/
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="buttons">
+      <button class="button is-primary">Primary</button>
+      <button class="button is-secondary">Secondary</button>
+      <button class="button is-link">Link</button>
+      <button class="button is-orange">Orange</button>
+    </div>
+
+    <div class="buttons">
+      <button class="button is-info">Info</button>
+      <button class="button is-success">Success</button>
+      <button class="button is-warning">Warning</button>
+      <button class="button is-danger">Danger</button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
